7c4688de3ea5047052ec772fd0c62887e88231e0
Bensonate <bensonate98@gmail.com>
2024-10-23 16:13:44 -0700

    cors configuration added

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const app = express();
 const connectDb = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
-const cors = require('cors');
+const fs = require("fs");
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
+// Load Swagger JSON file
+const swaggerDocument = JSON.parse(fs.readFileSync('./openapi.json', 'utf8'));
+// Serve Swagger UI at /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 const startServer = async ()=>{
